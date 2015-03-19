a4df5c1e4cefe8a692aa00217811060df0e498b8
Jay Won <breadmj@gmail.com>
2015-03-19 12:15:05 +0900

    feature_accept_charset 작업중

--- feature/feature_accept_charset.js
+++ feature/feature_accept_charset.js
@@ -1,8 +1,24 @@
+const charset_types = ['windows-949','windows-1258','utf-8','urf-8','utf-16','iso-8859-1','gb2312','gbk','euc-kr','*'];
+
 var feature_accept_charset = function(http_request) {
+
+    var feature_vector = [0,0,0,0,0,0,0,0,0,0];
 
     var lines = http_request.split('\n');
 
-    var feature_vector = [0,1,2];
+    lines.forEach(function(line) {
+        if(line.toLowerCase().startsWith('accept-charset')) {
+            try {
+                var types = line.split(':', 2)[1].split(',');
+                types.forEach(function(type) {
+                    type = type.trim().toLowerCase();
+                
+                });
+            } catch(e) {
+            
+            }
+        }
+    });
 
     return feature_vector;
 };
